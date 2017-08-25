797702c76b83cf50c41e939e9d8cb9114abae91e
CharlesGarrocho <charles.garrocho@gmail.com>
2017-08-25 14:35:46 -0300

    adicionado: projeto android.

---
+++ ClienteAndroid/IFGuide/app/src/main/java/ifspcjo/ifguide/AtividadeInicio.java
@@ -0,0 +1,13 @@
+package ifspcjo.ifguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AtividadeInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atividade_inicio);
+    }
+}
