60563730c8ea8ceb0fe271cd10b48f523581c72e
mashim <ma2shimazu@gmail.com>
2017-09-21 15:02:38 -0300

    mudancas no app

--- ClienteAndroid/IFGuide/app/src/main/java/ifspcjo/ifguide/AtividadeInicio.java
+++ ClienteAndroid/IFGuide/app/src/main/java/ifspcjo/ifguide/AtividadeInicio.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,19 +20,27 @@ import com.github.kittinunf.fuel.core.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import android.widget.*;
+import android.view.*;
+import android.app.*;
+
 public class AtividadeInicio extends AppCompatActivity {
     ListView lv;
     public ArrayList<String> titulos;
     public ArrayList<String> descricoes;
     public ArrayList<String> datas;
     public ArrayList<String> horas;
-
+    public EditText recebeCurso,recebeSerie;
+    public Button btenviar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.atividade_inicio);
+        recebeSerie = (EditText) findViewById(R.id.campoSerie);
+        recebeCurso = (EditText) findViewById(R.id.campoCurso);
+        btenviar = (Button) findViewById(R.id.btenviar);
 
-        Fuel.get("http://127.0.0.1:8080/eventos").responseString(new Handler<String>() {
+            Fuel.get("http://http://127.0.0.1:8080/eventos").responseString(new Handler<String>() {
             @Override
             public void failure(Request request, Response response, FuelError error) {
                 //do something when it is failure
@@ -40,34 +50,72 @@ public class AtividadeInicio extends AppCompatActivity {
             @Override
             public void success(Request request, Response response, String data) {
                 Log.e("sucesso", data);
-                try {
-                    JSONArray jsonEventos = new JSONArray(data);
-                    JSONObject evento;
-
-                    titulos = new ArrayList<String>();
-                    datas = new ArrayList<String>();
-                    descricoes = new ArrayList<String>();
-                    horas = new ArrayList<String>();
-
-                    for (int i=0; i<jsonEventos.length(); i++) {
-                        evento = new JSONObject(jsonEventos.getString(i));
-                        titulos.add(evento.getString("titulo"));
-                        datas.add(evento.getString("data"));
-                        descricoes.add(evento.getString("descricao"));
-                        horas.add(evento.getString("hora") + " as " + evento.getString("duracao"));
-
-                    }
-
-                }catch (Exception erro) {
-                    Log.e("ERROR", erro.getMessage());
-                }
-
+                filtro(data);
                 lv=(ListView)findViewById(R.id.lista);
                 Adaptador adaptador = new Adaptador(AtividadeInicio.this, titulos, descricoes, datas, horas);
                 lv.setAdapter(adaptador);
             }
         });
+        btenviar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                String serie = (recebeSerie.getText().toString());
+                String curso = (recebeCurso.getText().toString());
+                Fuel.get("http://http://127.0.0.1:8080/periodos?curso="+curso+"&serie="+serie).responseString(new Handler<String>() {
+                    @Override
+                    public void failure(Request request, Response response, FuelError error) {
+                        //do something when it is failure
+                        Log.e("erro", error.toString());
+                    }
 
+                    @Override
+                    public void success(Request request, Response response, String data) {
+                        Log.e("sucesso", data);
+                        JSONArray jsonPeriodos = new JSONArray(data);
+                        JSONObject periodos = new JSONObject(jsonPeriodos.getString(1));
+                        int id=(periodos.getString("id"));
+                        Fuel.get("http://http://127.0.0.1:8080/eventos?id="+id).responseString(new Handler<String>() {
+                            @Override
+                            public void failure(Request request, Response response, FuelError error) {
+                                //do something when it is failure
+                                Log.e("erro", error.toString());
+                            }
+
+                            @Override
+                            public void success(Request request, Response response, String data) {
+                                Log.e("sucesso", data);
+                                filtro(data);
+                                lv=(ListView)findViewById(R.id.lista);
+                                Adaptador adaptador = new Adaptador(AtividadeInicio.this, titulos, descricoes, datas, horas);
+                                lv.setAdapter(adaptador);
+                            }
+                    }
+                });
+            }
+        });
     }
+    public void filtro(String data){
+        try {
+            JSONArray jsonEventos = new JSONArray(data);
+            JSONObject evento;
+
+            titulos = new ArrayList<String>();
+            datas = new ArrayList<String>();
+            descricoes = new ArrayList<String>();
+            horas = new ArrayList<String>();
+
+            for (int i=0; i<jsonEventos.length(); i++) {
+                evento = new JSONObject(jsonEventos.getString(i));
+                titulos.add(evento.getString("titulo"));
+                datas.add(evento.getString("data"));
+                descricoes.add(evento.getString("descricao"));
+                horas.add(evento.getString("hora") + " as " + evento.getString("duracao"));
+
+            }
+
+        }catch (Exception erro) {
+            Log.e("ERROR", erro.getMessage());
+        }
+    }
+
 }
 
